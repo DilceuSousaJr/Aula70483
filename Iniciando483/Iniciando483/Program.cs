ccff58b1ce314ca41b0c42a8bce51449f2de2e1c
Dilceu Sousa Junior <sousajrdilceu@gmail.com>
2019-12-02 20:26:02 -0300

    Meu orimeiro commit no viusal studio

---
+++ Iniciando483/Iniciando483/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iniciando483
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola mundo");
+
+            Console.ReadKey(true);
+        }
+    }
+}
