9f3f945738d312ddaafcf1962c21e1597b1b390b
Dilceu Sousa Junior <sousajrdilceu@gmail.com>
2019-12-02 20:41:48 -0300

    Project git

---
+++ Meu projeto git/Meu projeto git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Meu_projeto_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Meu projeto git1/Meu projeto git1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Meu_projeto_git1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("update git");
+
+            Console.ReadKey(true);
+        }
+    }
+}
