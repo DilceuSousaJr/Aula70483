b079f8a6b0f9dca168aaad5b652a585dcf647ce0
Dilceu Sousa Junior <sousajrdilceu@gmail.com>
2019-12-02 21:56:37 -0300

    lista de carros

---
+++ ListaComNomesDeCarros/ListaComNomesDeCarros/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaComNomesDeCarros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<string> carros = new List<string>;
+            {
+                "Megane",
+                "s10",
+                "Jetta",
+                "Golf",
+            };
+
+            carros.ForEach(x => Console.WriteLine(x));
+
+            Console.ReadKey(true);
+        }
+    }
+}
