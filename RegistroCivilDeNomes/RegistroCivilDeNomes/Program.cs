76faee64fad8daf0a094d2a1c413f6bc87eb2040
Dilceu Sousa Junior <sousajrdilceu@gmail.com>
2019-12-03 21:59:07 -0300

    commit

--- RegistroCivilDeNomes/RegistroCivilDeNomes/Program.cs
+++ RegistroCivilDeNomes/RegistroCivilDeNomes/Program.cs
@@ -11,9 +11,7 @@ namespace RegistroCivilDeNomes
         static List<string> ListaDeNomes = new List<string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("***********************");
-            Console.WriteLine("**SISTEMA DE REGISTRO**");
-            Console.WriteLine("***********************");
+
 
 
 
@@ -21,35 +19,18 @@ namespace RegistroCivilDeNomes
 
             while(menuEscolhido != "5")
             {
-                Console.WriteLine("Digite o numero para opção desejada:");
-                Console.WriteLine("1 - Registrar Nome");
-                Console.WriteLine("2 - Listar Nomes");
-                Console.WriteLine("3 - Editar Um Nome");
-                Console.WriteLine("4 - Excluir Nomes");
-                Console.WriteLine("5 - Sair do Sistema");
+                menuEscolhido = MostrarMenu();
 
                 menuEscolhido = Console.ReadLine();
+
 
                 switch (menuEscolhido)
                 {
                     case "1":
-                        Console.WriteLine("Registro de Nome Selecionado");
-                        Console.WriteLine("Informa um Nome:");
-                        var nomeInformado = Console.ReadLine();
-
-
-                        ListaDeNomes.Add(nomeInformado);
-
-                        Console.WriteLine("Nome Informado com Sucesso!");
-                        Console.ReadKey(true);
+;
                         break;
                     case "2":
-                        Console.WriteLine("Listagem de Nome Selecionado");
 
-                        ListaDeNomes.ForEach(x => Console.WriteLine($"Nome: {x}"));
-
-                        Console.WriteLine("Listagem de nomes finalizada");
-                        Console.ReadKey(true);
                         break;
                     default:
                         Console.WriteLine("Menu Invalido");
@@ -60,6 +41,45 @@ namespace RegistroCivilDeNomes
             }
 
             Console.ReadKey(true);
+        }
+
+        static void MensagemInicial()
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("**SISTEMA DE REGISTRO**");
+            Console.WriteLine("***********************");
+        }
+        static void ListarNome()
+        {
+            Console.WriteLine("Listagem de Nome Selecionado");
+
+            ListaDeNomes.ForEach(x => Console.WriteLine($"Nome: {x}"));
+
+            Console.WriteLine("Listagem de nomes finalizada");
+            Console.ReadKey(true);
+        }
+        static void RegistrarNome()
+        {
+            Console.WriteLine("Registro de Nome Selecionado");
+            Console.WriteLine("Informa um Nome:");
+            var nomeInformado = Console.ReadLine();
+
+
+            ListaDeNomes.Add(nomeInformado);
+
+            Console.WriteLine("Nome Informado com Sucesso!");
+            Console.ReadKey(true);
+        }
+        static string MostrarMenu()
+        {
+            Console.WriteLine("Digite o numero para opção desejada:");
+            Console.WriteLine("1 - Registrar Nome");
+            Console.WriteLine("2 - Listar Nomes");
+            Console.WriteLine("3 - Editar Um Nome");
+            Console.WriteLine("4 - Excluir Nomes");
+            Console.WriteLine("5 - Sair do Sistema");
+
+            return Console.ReadLine();
         }
     }
 }
