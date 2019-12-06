47dc9fe03e84d6b406746f61659a3ed497079d39
Dilceu Sousa Junior <sousajrdilceu@gmail.com>
2019-12-05 22:00:08 -0300

    commit Filme

---
+++ IniciandoWpf/IniciandoWpf/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IniciandoWpf
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataGrid.ItemsSource = ListaDeNomes;
+        }
+
+        List<string> ListaDeNomes = new List<string>
+        {
+          "Chael",
+          "Rocneth",
+          "Liamcyn",
+          "Dicjohn",
+          "Dahe",
+          "Chellren",
+          "Kimni",
+          "Hadod",
+          "Garnyahildi",
+          "Reginisen",
+          "Danmax",
+          "Leysig",
+          "Hesfastnath",
+          "Jenba",
+          "Marmaand",
+          "Gorma",
+          "Helm",
+          "Isumlin",
+          "Jenann",
+          "Doegar",
+        };
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataGrid.ItemsSource = null;
+
+
+            switch (cbxFiltro.SelectedIndex)
+            {
+                case 0:
+                    OrdenarLista();
+                    break;
+                case 1:
+                    FiltarPorLetras(tbxFiltro.Text);
+                    break;
+                case 2:
+                    OrdenarMaiorParaMenor();
+                    break;
+
+            }
+
+        }
+
+        private void OrdenarLista()
+        {
+            DataGrid.ItemsSource = ListaDeNomes.OrderBy(x => x);
+        }
+
+        private void FiltarPorLetras(string parametro)
+        {
+            DataGrid.ItemsSource = ListaDeNomes.Where(x => x.Contains("a"));
+        }
+
+        private void OrdenarMaiorParaMenor()
+        {
+            DataGrid.ItemsSource = ListaDeNomes.OrderByDescending(x => x.Length);
+        }
+    }
+}
---
+++ filmesWPF/filmesWPF/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IniciandoWpf
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataGrid.ItemsSource = ListaDeNomes;
+        }
+
+        List<string> ListaDeNomes = new List<string>
+        {
+            "O Justiceiro", 
+            "Busca Implacável", 
+            "O Virgem de 40 Anos", 
+            "Em Ritmo de Fuga", 
+            "Operação Invasão", 
+            "Rota de Fuga",
+            "Código de Conduta", 
+            "Os Oito Odiados", 
+        };
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataGrid.ItemsSource = null;
+
+
+            switch (cbxFiltro.SelectedIndex)
+            {
+                case 0:
+                    OrdenarLista();
+                    break;
+                case 1:
+                    FiltarPorLetras(tbxFiltro.Text);
+                    break;
+                case 2:
+                    OrdenarMaiorParaMenor();
+                    break;
+
+            }
+
+        }
+
+        private void OrdenarLista()
+        {
+            DataGrid.ItemsSource = ListaDeNomes.OrderBy(x => x);
+        }
+
+        private void FiltarPorLetras(string parametro)
+        {
+            DataGrid.ItemsSource = ListaDeNomes.Where(x => x.Contains("a"));
+        }
+
+        private void OrdenarMaiorParaMenor()
+        {
+            DataGrid.ItemsSource = ListaDeNomes.OrderByDescending(x => x.Length);
+        }
+
+        private void CbxFiltro_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
