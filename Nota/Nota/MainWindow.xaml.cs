10049ab6e1191fb56b4e36016828864ff287fc08
Dilceu Sousa Junior <sousajrdilceu@gmail.com>
2019-12-10 21:56:51 -0300

    commit aula 6

---
+++ Nota/Nota/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Nota.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nota
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Aluno> Alunos = new List<Aluno>();
+
+        private void Cadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            Alunos.Add(new Aluno()
+            {
+                Nome = tbxNome.Text,
+                Nota1 = int.Parse(tbxNota1.Text),
+                Nota2 = int.Parse(tbxNota2.Text),
+                Nota3 = int.Parse(tbxNota3.Text),
+            });
+
+            dataGrid.ItemsSource = null;
+            dataGrid.ItemsSource = Alunos;
+        }
+    }
+}
